0431f736e277634a4d6be49210d9d1eb55a1d8eb
nenemizc <emarcellm@gmail.com>
2026-09-18 10:23:21 +0200

    1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,50 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service.js';
+import { Criminal } from './criminal.js';
+import fs from 'node:fs';
+import { get } from 'node:http';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
   @Render('index')
   getHello() {
     return {
-      title: 'My First NestJS App'
+      title: 'My First NestJS App',
+    };
+  }
+  @Get('/piros-kek')
+  @Render('red-blue')
+  getRedBlue() {
+    const random = Math.random();
+    const bgColor = random > 0.5 ? 'red' : 'blue';
+    return {
+      bgColor,
+    };
+  }
+  @Get('wanted')
+  @Render('wanted')
+  getWanted() {
+    const criminal = JSON.parse(
+      fs.readFileSync('wanted.json', { encoding: 'utf-8' }),
+    ) as Criminal;
+    return { criminal };
+  }
+  @Get('search')
+  @Render('search')
+  searchCrime(@Query('keresett') keresett: string) {
+    if (!keresett) {
+      return {
+        talalatok: [],
+      };
     }
+    const criminal = JSON.parse(
+      fs.readFileSync('wanted.json', { encoding: 'utf-8' }),
+    ) as Criminal;
+
+    return {
+      talalatok: criminal.crimes.filter((crime) =>
+        crime.toLowerCase().includes(keresett.toLowerCase()),
+      ),
+    };
   }
 }
---
+++ src/criminal.ts
@@ -0,0 +1,11 @@
+export interface Criminal {
+  name: string
+  profile: string
+  age: number
+  hair: string
+  eye: string
+  lastSeenDate: string
+  lastSeenAt: string
+  crimes: string[]
+  hobbies: string[]
+}
